1fcb55441b7add13a311f3fbba063322802f5e0b
Praveen <praveendharman556@gmail.com>
2025-08-27 13:49:06 +0530

    Create test project

---
+++ Test-Fork/Test_ForkApp.swift
@@ -0,0 +1,17 @@
+//
+// Untitled 1
+// Copyright © ___ORGANIZATIONNAME___ 2025 
+// MIT license, see LICENSE.md file for details
+// 
+        
+
+import SwiftUI
+
+@main
+struct Test_ForkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
