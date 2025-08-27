9740265fca72e1e3b0bbd8dc52526d7f11980111
Praveen <praveendharman556@gmail.com>
2025-08-27 13:49:16 +0530

    Added content view

---
+++ Test-Fork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+// Untitled 1
+// Copyright © ___ORGANIZATIONNAME___ 2025 
+// MIT license, see LICENSE.md file for details
+// 
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
